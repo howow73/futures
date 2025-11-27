356a0d012319c03a7255d1d1ee128b3540b56894
Local User <local@example.com>
2025-11-27 14:58:33 +0900

    캡처 기능 추가

--- src/util/mouse_watch.py
+++ src/util/mouse_watch.py
@@ -1,9 +1,10 @@
 import time, pyautogui
+
 print("Move the mouse. Press Ctrl+C to stop.")
 try:
     while True:
         x, y = pyautogui.position()
         print(f"X={x} Y={y}", end="\r", flush=True)
         time.sleep(0.05)
-except KeyboardInterrupt:
+except KeyboardInterrupt: 
     print("\nStopped.")
